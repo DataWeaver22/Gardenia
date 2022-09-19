45d26ee82f33a5858221157da835c2765b6f58e7
ADMIN <ADMIN@DESKTOP-EFETHMT>
2022-09-19 15:51:28 +0530

    logo and sidenav

--- Gardenia/src/main/java/com/example/demo/security/SecurityConfiguration.java
+++ Gardenia/src/main/java/com/example/demo/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -32,8 +33,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 				.antMatchers("/**").permitAll()
 				.and().formLogin();
 		
+		
 		http.cors().and().csrf().disable();
 	}
+	
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**");      
+        web.ignoring().antMatchers("/scripts/**");
+        web.ignoring().antMatchers("/images/**");
+    }
 	
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
