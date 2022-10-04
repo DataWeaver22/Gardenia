47ceec0d5be707a064a57cfcadc62896bc28ea94
ADMIN <ADMIN@DESKTOP-EFETHMT>
2022-10-04 11:39:18 +0530

    War File and Login Page

--- Gardenia/src/main/java/com/example/demo/GardeniaApplication.java
+++ Gardenia/src/main/java/com/example/demo/GardeniaApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GardeniaApplication {
+public class GardeniaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GardeniaApplication.class, args);
 	}
-
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GardeniaApplication.class);
+    }
 }
